bcf5b4a61735fddfc1301e621190eb4ddb2a65e3
Alex <alexbonniard@gmail.com>
2024-12-22 02:30:32 +0100

    Initial commit

---
+++ backend/routes/book.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bookController = require('../controllers/book');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
+
+router.post('/', auth, multer, bookController.createBook);
+router.get('/', bookController.getBooks);
+router.get('/bestrating', bookController.getBestRating);
+router.put('/:id', auth, multer, bookController.updateBook);
+router.get('/:id', bookController.getBookById);
+router.delete('/:id', auth, bookController.deleteBook);
+
+module.exports = router;
